42b475f9446e1ba578762ae9247e59c58aabcf8a
Fred Zirdung <fred@hackreactor.com>
2019-03-07 05:16:46 -0800

    (init)

---
+++ client/js/httpHandler.js
@@ -0,0 +1,5 @@
+(function() {
+
+  const serverUrl = 'http://127.0.0.1:3000';
+
+})();
---
+++ server/js/httpHandler.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const headers = require('./cors');
+const multipart = require('./multipartUtils');
+
+// Path for the background image ///////////////////////
+module.exports.backgroundImageFile = './background.jpg';
+////////////////////////////////////////////////////////
+
+module.exports.router = (req, res, next = ()=>{}) => {
+  console.log('Serving request type ' + req.method + ' for url ' + req.url);
+  res.writeHead(200, headers);
+  res.end();
+};
